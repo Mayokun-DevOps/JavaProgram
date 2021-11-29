293f81cee4585b139d97d0fa5f24b62a1d915dc4
Mayor-Linux <sogsax@gmail.com>
2021-11-29 15:48:15 +0100

    2nd java program

---
+++ HelloDevOps.java
@@ -0,0 +1,7 @@
+// My Second Program
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, Devops guys" to the terminal window
+        System.out.println("Hello, Devops Guy's world!"); 
+    }
+}
